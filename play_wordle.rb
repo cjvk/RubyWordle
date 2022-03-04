4617effc846fa4341987cac3a3e69cb7b85db902
Christopher von Krogh <code@getbraintree.com>
2022-03-04 10:52:05 -0800

    hints, printing, quit

--- play_wordle.rb
+++ play_wordle.rb
@@ -17,17 +17,62 @@ def play(d)
   puts "Welcome to wordle!"
   for guess in 1..6
     puts "You are on guess #{guess}/6. There are #{d.size} matching words remaining."
-    print "Enter your guess, or PRINT: ==> "
-    word = gets.chomp
-    if word == "PRINT"
-      d.each do |key, value|
-        puts key
+    while true do
+      print "Enter a guess, or (p)rint, (h)int, (q)uit: ==> "
+      choice = gets.chomp
+      case choice
+      when "p"
+        max_print = 30
+        d.each do |key, value|
+          puts key
+          break if (max_print = max_print - 1) == 0
+        end
+      when "h"
+        hint(d)
+      when "q"
+        return
+      else # assume anything else is a guess
+        word = choice
+        puts "word=#{word}"
+        print "Enter the response (!?-): ==> "
+        response = gets.chomp
+        filter(d, word, response)
+        break
       end
-      return
     end
-    print "Enter the response (!?-): ==> "
-    response = gets.chomp
-    filter(d, word, response)
+  end
+end
+
+def hint(d)
+  num_remaining = d.size
+  puts "remaining: #{num_remaining}"
+  character_dictionary = {}
+  for c in 97..122
+    character_dictionary[c.chr] = 0
+  end
+  d.each do |word, line_num|
+    character_dictionary.each do |c, num_so_far|
+      if word[c]
+        # puts "found #{c} in #{word}"
+        character_dictionary[c] = num_so_far + 1
+      end
+    end
+  end
+  top_n = 3
+  top_n_dict = {}
+  for i in 0...top_n
+    puts i
+    next_largest = character_dictionary.max_by{|k,v| (v==num_remaining||top_n_dict.has_key?(k)) ? 0 : v}
+    top_n_dict[next_largest[0]]=next_largest[1]
+  end
+  puts top_n_dict
+  d.each do |word, line_num|
+    count = 0
+    top_n_dict.each do |c, num_occurrences|
+      count = count + 1 if word[c]
+    end
+    puts "#{word} is a GREAT guess" if count == top_n
+    puts "#{word} is a good guess" if count == (top_n-1)
   end
 end
 
