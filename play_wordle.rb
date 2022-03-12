08d0900b8dd6bfbd80d958d41c65cbad1eb11594
Christopher von Krogh <code@getbraintree.com>
2022-03-11 16:07:29 -0800

    problematic pattern alert

--- play_wordle.rb
+++ play_wordle.rb
@@ -17,6 +17,9 @@ def play(d)
   puts "Welcome to wordle!"
   for guess in 1..6
     puts "You are on guess #{guess}/6. There are #{d.size} matching words remaining."
+    if guess >= 3
+      check_for_problematic_patterns(d)
+    end
     while true do
       print "Enter a guess, or (p)rint, (h)int, (q)uit: ==> "
       choice = gets.chomp
@@ -46,9 +49,44 @@ def play(d)
   end
 end
 
+def close(w1, w2)
+  diff = 0
+  for i in 0...5
+    diff = diff + (w1[i]==w2[i] ? 0 : 1)
+  end
+  diff == 1
+end
+
+def check_for_problematic_patterns(d)
+  # e.g. Wordle 265 ("watch"), after raise-clout (-!---, ?---?)
+  # legal words remaining: watch, match, hatch, patch, batch, natch, tacky
+  # 6 words with _atch, plus tacky
+  pp_dict = {}
+  d.each do |key1, value1|
+    found = false
+    pp_dict.each do |key2, value2|
+      if close(key1, key2)
+        found = true
+        pp_dict[key2] = value2 + 1
+      end
+    end
+    if !found
+      pp_dict[key1] = 1
+    end
+  end
+  pp_dict.each do |key, value|
+    if value > 2
+      puts ""
+      puts "PROBLEMATIC PATTERN ALERT: found \"#{key}\" with #{value} matching words (print for details)"
+      puts ""
+    end
+  end
+end
+
 def hint(d)
   num_remaining = d.size
   puts "remaining: #{num_remaining}"
+  # character_dictionary stores, for each letter, the number of remaining words with that letter
   character_dictionary = {}
   for c in 97..122
     character_dictionary[c.chr] = 0
@@ -61,19 +99,22 @@ def hint(d)
     end
   end
   top_n = 3
+  # top_n_dict will contain the top N keys from character_dictionary, by count
   top_n_dict = {}
   for i in 0...top_n
     next_largest = character_dictionary.max_by{|k,v| (v==num_remaining||top_n_dict.has_key?(k)) ? 0 : v}
     top_n_dict[next_largest[0]]=next_largest[1]
   end
   puts top_n_dict
+  # for all remaining words, they are a great guess if all of the "top N" characters are contained
+  # and they are a "good" guess if all but one of the top N characters occur
   d.each do |word, line_num|
     count = 0
     top_n_dict.each do |c, num_occurrences|
       count = count + 1 if word[c]
     end
     puts "#{word} is a GREAT guess" if count == top_n
-    puts "#{word} is a good guess" if count == (top_n-1)
+    puts "#{word} is a good guess" if count == (top_n - 1)
   end
 end
 
